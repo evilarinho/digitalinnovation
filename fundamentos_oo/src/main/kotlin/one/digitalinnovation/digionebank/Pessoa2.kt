d7c9e0569e5ba8e7884ffb49fe7bf9842428ce1a
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-06-28 23:07:56 -0300

    classe pessoa1.kt

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa2.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa2 {
+    var nome: String = "Edilson"
+    var cpf: String = "123.123.123-00"
+}
+
+fun main() {
+    val edilson = Pessoa2()
+
+    println(edilson.nome)
+    println(edilson.cpf)
+}
