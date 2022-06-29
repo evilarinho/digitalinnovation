7bd5828746739f0807b7bf8f3b8ea7873c2a8452
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-06-29 18:39:22 -0300

    create Conta2 class with primary constructor

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Conta2.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.digionebank.testes
+
+import java.math.BigDecimal
+
+class Conta2(
+    val agencia: String
+    val numero: String
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+    }
+}
+
+
+
