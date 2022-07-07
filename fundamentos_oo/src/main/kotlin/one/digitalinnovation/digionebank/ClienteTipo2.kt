8bb3aa398fdcdff3023519b6bbb6b121a29fb282
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-07-06 22:21:57 -0300

    created enum client type and test to it

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo2.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.digionebank
+
+enum class ClienteTipo2(val descricao: String) {
+    PF("Pessoa Física"),
+    PJ("Pessoa Jurídica")
+}
---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoenum2.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.ClienteTipo2
+
+fun main() {
+    ClienteTipo2.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo2.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo2.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo2.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
+}
