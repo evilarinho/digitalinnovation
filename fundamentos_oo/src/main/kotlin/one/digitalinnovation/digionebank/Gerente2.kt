99fc6a3c3140828fb78bdbb285cc307b8f5401ab
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-07-13 16:16:14 -0300

    created sub classes manager and analyst

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Gerente2.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Gerente2.kt
@@ -1,4 +1,9 @@
 package one.digitalinnovation.digionebank
 
-class Gerente2 {
+class Gerente2(
+    nome: String,
+    cpf: String,
+    salario: Double)
+    : Funcionario2(nome = nome, cpf = cpf, salario = salario) {
+    override fun calculoAuxilio2(): Double = salario * 0.4
 }
