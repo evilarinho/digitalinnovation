40fc0f3fa37509f21aa8afa0cdca551f56314176
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-07-15 16:52:21 -0300

    created string array of test class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteArrayString2.kt
@@ -0,0 +1,21 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val nomes = Array(size = 3) { "" }
+    nomes[0] = "Maria"
+    nomes[1] = "Zezé"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("---------------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("---------------------------")
+    val nomes2 = arrayOf("Maria", "Zezè", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
