e99d2ac45ccc5060386b0ec244433bd1ea00bad6
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-07-21 13:22:37 -0300

    Development with mutableMapOf - Part 1- Repositorio2.kt TesteMutavelMap2.kt

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteMutavelMap2.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario2(nome = "Joao", salario = 2000.0, tipoContratacao = "CLT")
+    val pedro = Funcionario2(nome = "Pedro", salario = 1500.0, tipoContratacao = "PJ")
+    val maria = Funcionario2(nome = "Maria", salario = 4000.0, tipoContratacao = "CLT")
+
+    val repositorio = Repositorio2<Funcionario2>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(joao.nome))
+    println(repositorio.findById(pedro.nome))
+    println(repositorio.findById(maria.nome))
+
+}
